0b0b4fc1e4ccf75cc537652a95465315ace63efb
Alexandr <axkuz97@yandex.ru>
2021-02-27 22:58:56 +0300

    gh pages build

---
+++ precache-manifest.5333414e20700d34c90d6e4bdc6f0899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70796b6fba97dd14caab1f142aa71f27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fdec02f3ff871dd7f96c",
+    "url": "./static/css/main.e61768ce.chunk.css"
+  },
+  {
+    "revision": "3c0ea6a025852998a1e0",
+    "url": "./static/js/2.f2d4b919.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.f2d4b919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdec02f3ff871dd7f96c",
+    "url": "./static/js/main.15027e86.chunk.js"
+  },
+  {
+    "revision": "90dc22d7c1e92fe1d967",
+    "url": "./static/js/runtime-main.8af312ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
